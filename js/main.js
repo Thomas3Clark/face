5b2809aa59ba380a6d37774a732998e975ec31ed
Zameryth <zameryth+github@gmail.com>
2016-02-29 05:10:35 -0800

    Updates

--- js/main.js
+++ js/main.js
@@ -19,11 +19,13 @@ function loadOptions() {
   var $gpsOn = $('#gpsOn');
   var $city = document.getElementById('city');
   var $tempFormat = document.getElementById('tempFormat');
+  var $apiid = document.getElementById('apiid');
 
   if (localStorage.weather) {
     $weatherOn[0].checked = localStorage.weather === 'true';
     $gpsOn[0].checked = localStorage.gps === 'true';
     $city.value = localStorage.manual_city;
+    $apiid.value = localStorage.weather_api;
     $tempFormat.value = localStorage.temperature_format;
   }
 }
@@ -33,18 +35,21 @@ function getAndStoreConfigData() {
   var $gpsOn = $('#gpsOn');
   var $city = document.getElementById('city');
   var $tempFormat = document.getElementById('tempFormat');
+  var $apiid = document.getElementById('apiid');
 
   var options = {
     weather: $weatherOn[0].checked,
     gps: $gpsOn[0].checked,
     manual_city: $city.value,
     temperature_format: $tempFormat.value,
+    weather_api: $apiid.value,
   };
 
   localStorage.weather = options.weather;
   localStorage.gps = options.gps;
   localStorage.manual_city = options.manual_city;
   localStorage.temperature_format = options.temperature_format;
+  localStorage.weather_api = options.weather_api;
 
   console.log('Got options: ' + JSON.stringify(options));
   return options;
