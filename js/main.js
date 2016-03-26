d7c17a1d41082a029abcbde715ffb69c080ed0f4
Zameryth <zameryth+github@gmail.com>
2016-03-25 18:47:09 -0700

    Update

--- js/main.js
+++ js/main.js
@@ -20,13 +20,17 @@ function loadOptions() {
   var $city = document.getElementById('city');
   var $tempFormat = document.getElementById('tempFormat');
   var $apiid = document.getElementById('apiid');
+  var $emblem = document.getElementById('emblem');
+  var $themeColor = document.getElementById('themeColor');
 
   if (localStorage.weather) {
-    $weatherOn[0].checked = localStorage.weather === 'true';
-    $gpsOn[0].checked = localStorage.gps === 'true';
-    $city.value = localStorage.manual_city;
-    $apiid.value = localStorage.weather_api;
-    $tempFormat.value = localStorage.temperature_format;
+    $weatherOn[0].checked = window.localStorage.weather === 'true';
+    $gpsOn[0].checked = window.localStorage.gps === 'true';
+    $city.value = window.localStorage.manual_city;
+    $apiid.value = window.localStorage.weather_api;
+    $tempFormat.value = window.localStorage.temperature_format;
+    $emblem.value = window.localStorage.emblem;
+    $themeColor.value = window.localStorage.themeColor;
   }
 }
 
@@ -36,6 +40,8 @@ function getAndStoreConfigData() {
   var $city = document.getElementById('city');
   var $tempFormat = document.getElementById('tempFormat');
   var $apiid = document.getElementById('apiid');
+  var $emblem = document.getElementById('emblem');
+  var $themeColor = document.getElementById('themeColor');
 
   var options = {
     weather: $weatherOn[0].checked,
@@ -43,13 +49,17 @@ function getAndStoreConfigData() {
     manual_city: $city.value,
     temperature_format: $tempFormat.value,
     weather_api: $apiid.value,
+    emblem: $emblem.value,
+    themeColor: $themeColor.value,
   };
 
-  localStorage.weather = options.weather;
-  localStorage.gps = options.gps;
-  localStorage.manual_city = options.manual_city;
-  localStorage.temperature_format = options.temperature_format;
-  localStorage.weather_api = options.weather_api;
+  window.localStorage.weather = options.weather;
+  window.localStorage.gps = options.gps;
+  window.localStorage.manual_city = options.manual_city;
+  window.localStorage.temperature_format = options.temperature_format;
+  window.localStorage.weather_api = options.weather_api;
+  window.localStorage.themeColor = options.themeColor;
+  window.localStorage.emblem = options.emblem;
 
   console.log('Got options: ' + JSON.stringify(options));
   return options;
